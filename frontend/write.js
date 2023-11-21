9301a848f91cd60780bc3db672feee494cbe4da6
LeeHareum <029843507@hanmail.net>
2023-11-21 14:47:00 +0900

    ✍️글쓰기 페이지 기능 구현
    - FE 글쓰기페이지Form작업
    - BE와SQLite 연동하여 이미지 및 데이터 업로드 기능 구현

--- frontend/write.js
+++ frontend/write.js
@@ -2,10 +2,13 @@ const form = document.getElementById("write-form");
 
 const handleSubmitForm = async (event) => {
   event.preventDefault();
+
+  const body = new FormData(form);
+  body.append("insertAt", new Date().getTime());
   try {
     const res = await fetch("/items", {
       method: "POST",
-      body: new FormData(form),
+      body,
     });
     const data = await res.json();
     if (data === "200") window.location.pathname = "/"; //정상처리되었을때 보여줄 창
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI,UploadFile,Form
+from fastapi import FastAPI,UploadFile,Form,Response
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 import sqlite3
@@ -13,15 +15,38 @@ app = FastAPI()
 async def create_item(image:UploadFile,
                 title:Annotated[str,Form()],
                 price:Annotated[int,Form()],
-                description:Annotated[str,Form()],
-                place:Annotated[str,Form()]):
+                description:Annotated[str,Form()], 
+                place:Annotated[str,Form()],
+                insertAt:Annotated[int,Form()]
+                ):
     image_bytes = await image.read() #image는 큰 데이터로 오기때문에 읽는과정에서 await이 필요
     # SQL문법▼items테이블중(title,image,price,description,place)에 VALUES ('{title}','{image_bytes.hex()}','{price}','{description}','{place}')값을 넣겠다#
     cur.execute(f"""
-                INSERT INTO items(title,image,price,description,place) 
-                VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}')
-                """)
+                INSERT INTO 
+                items(title,image,price,description,place,insertAt) 
+                VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}',{insertAt}) 
+                """) #SQL
     con.commit() #데이터들어가짐
     return '200'
+
+@app.get('/items')
+async def get_items():  #컬럼명도 같이가져옴
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    rows = cur.execute(f"""
+                       SELECT * from items;
+                       """).fetchall()
+    return JSONResponse(jsonable_encoder(dict(row)for row in rows)) #dict_객체형태로바꿔줌
+
+
+@app.get('/images/{item_id}')
+async def get_image(item_id):
+    cur = con.cursor()
+    image_bytes = cur.execute(f"""
+                              SELECT image from items WHERE id={item_id}
+                              """).fetchone()[0]  #.fetchone()[0] 한개만 불러오는
+    return Response(content=bytes.fromhex(image_bytes))  #img를가져와서 컨텐츠로변환해주는
+
+
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
